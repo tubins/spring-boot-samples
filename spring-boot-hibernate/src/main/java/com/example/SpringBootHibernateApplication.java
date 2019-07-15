73a1f0353694fbb2b2bf82dc8a6d3953f3b2b6f1
Tubin <tubin.shaji@citrusinformatics.com>
2019-07-15 16:27:41 +0530

    Enable component scan
    Using a root package also allows the @ComponentScan annotation
    to be used without needing to specify a basePackage attribute

--- spring-boot-hibernate/src/main/java/com/example/SpringBootHibernateApplication.java
+++ spring-boot-hibernate/src/main/java/com/example/SpringBootHibernateApplication.java
@@ -2,8 +2,10 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class SpringBootHibernateApplication {
 
 	public static void main(String[] args) {
